a3e87aa8f602181881abffb3c24e76c2aa4ef190
JINZHANG2017 <zj574406254@hotmail.com>
2020-10-21 15:35:07 +0800

    finish original css html design

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
 import "./style/index.scss";
+
+// fetchData=(url)=> {
+//     // <-- start
+//     // TODO 23: 通过Fetch API实现异步请求
+//     // end -->
+//     return fetch(url)
+//       .then(function(response) {
+//         return response.json();
+//       })
+//       .catch(function(e) {
+//         return e;
+//       });
+//   }
