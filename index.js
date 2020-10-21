3dac21b821142497f6bd6b5a48dac281821adad1
JINZHANG2017 <zj574406254@hotmail.com>
2020-10-21 16:45:01 +0800

    [ZhangJin] feat: add display for educations

--- index.js
+++ index.js
@@ -16,10 +16,32 @@ function fetchData(url) {
 }
 
 $(function () {
-  const url = "http://localhost:8080/users/1";
+  const currentUrl = window.location.href;
+  const splitArray = currentUrl.split("/");
+  //   console.log(splitArray);
+  const requestId = splitArray[splitArray.length - 1];
+  const url = `http://localhost:8080/users/${requestId}`;
   fetchData(url)
     .then((result) => {
+      if (result.message) {
+        console.log(result.message);
+        return;
+      }
       console.log(result);
+      $(".name").html(result.name);
+      $(".age").html(result.age);
+      $(".user-description").html(result.description);
+      result.educations.forEach((value) => {
+        const itemStr = `<li class="educations-items">
+          <div class="year">${value.year}</div>
+          <div>
+            <header class="education-header">${value.title}</header>
+            <div class="education-description">${value.description}</div>
+          </div>
+        </li>`;
+        $(".educations-ul").append(itemStr);
+      });
+      //   console.log(result);
     })
     .catch((error) => {
       console.error(error);
